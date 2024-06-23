d447b2af393750d49a49ae7e2512eaf816fb26f2
Bruno Caitano <snowrunescape@snowdev.com.br>
2024-06-23 12:35:16 -0400

    feat: index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+declare module 'cart-ts' {
+  export type cart = {
+    list(): Item[],
+    get(id: number | string): Item | undefined,
+    add(item: Item): void,
+    remove(id: number | string): void,
+    exists(id: number | string): boolean,
+    total(): number,
+    clean(): void,
+  }
+
+  export type Item = {
+    id: number | string,
+    price: number,
+    quantity: number,
+    metadata?: Record<string, any>,
+    [key: string]: any,
+  }
+}
